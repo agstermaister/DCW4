ec1be99136b4e079d7afc281cd8405cf2dae553d
agstermaister <131816758+agstermaister@users.noreply.github.com>
2024-02-21 21:00:09 +0000

    Update run_analysis.R
    update code

--- run_analysis.R
+++ run_analysis.R
@@ -4,6 +4,8 @@ library(dplyr)
 
 # Set directory 
 setwd("data")
+
+#Import features.txt to use as column headers
 features <- read.table("features.txt", sep = "\t")
 
 # Read training and test datasets
@@ -25,8 +27,8 @@ combined_test_data <- cbind(test_subject_data, test_y_data,test_x_data)
 # Switch back to the original working directory
 setwd(current_dir)
 # Set the working directory to the "train" folder
-trainign_path <- file.path(current_dir, training_folder_name)
-setwd(trainign_path)
+training_path <- file.path(current_dir, training_folder_name)
+setwd(training_path)
 
 train_subject_data <- read.table("subject_train.txt", header = FALSE, sep = "\t")
 train_y_data <- read.table("y_train.txt", sep = "\t")
@@ -45,8 +47,7 @@ combined_df$Measures <- gsub("\\s+", " ", combined_df$Measures)
 split_names <- do.call(rbind, strsplit(as.character(combined_df$Measures), " "))
 combined_df <- cbind(combined_df, split_names)
 
-# Use descriptive activity names to name the activities in the data set
-col_names2 <- features$V1
+# Label the data set with descriptive variable names
 col_names <- c(new_col_names,"blank",col_names2)
 colnames(combined_df) <- col_names
 
@@ -54,7 +55,7 @@ colnames(combined_df) <- col_names
 short_combined_df <- combined_df %>%
         select(matches("Subject ID|Activity|mean|sdv"))
 
-# Label the data set with descriptive variable names
+# Use descriptive activity names to name the activities in the data set
 short_combined_df <- short_combined_df %>% mutate(Activity = ifelse(Activity == 1, "WALKING", ifelse(Activity == 2, "WALKING_UPSTAIRS", ifelse(Activity == 3, "WALKING_DOWNSTAIRS", ifelse(Activity == 4, "SITTING", ifelse(Activity == 5, "STANDING", "LAYING"))))))
 
 # Create new data set with the average of each variable
